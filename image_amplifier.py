ebbc9c013893ccc046a56b9829fa31e1067b28a6
legendmaker <billywty@umich.edu>
2022-04-14 15:10:40 -0400

    denoise ultimate

--- image_amplifier.py
+++ image_amplifier.py
@@ -6,12 +6,11 @@ import numpy as np
 #ratio: how large to amplify
 #pos = 'LR', 'LL', 'UR', 'UL' where to put the amplified image
 #Usage: amplify_rec('data/denoising/image_Lena512rgb.png', (150, 70), (250, 130), 'LR')
-def amplify_rec(filepath, pt1, pt2, ratio = 2,  pos = 'LR'):
+def amplify_rec(filepath, destpath, pt1, pt2, ratio = 2,  pos = 'LR'):
     image = cv2.imread(filepath)
     cv2.rectangle(image, pt1, pt2, (0, 0, 255), 2)
     patch1 = image[pt1[1]:pt2[1], pt1[0]:pt2[0], :]
     patch1 = cv2.resize(patch1, (ratio*patch1.shape[1], 2*patch1.shape[0]))
-    cv2.imwrite('patch.png', patch1)
     width = patch1.shape[1]
     height = patch1.shape[0]
     if pos == 'UL':  #UPPER LEFT
@@ -23,6 +22,10 @@ def amplify_rec(filepath, pt1, pt2, ratio = 2,  pos = 'LR'):
     else:
         image[image.shape[0]-height:image.shape[0], image.shape[1]-width:image.shape[1]] = patch1
     cv2.imshow('demo', image)
-    cv2.imwrite('test.png', image)
+    cv2.imwrite(destpath, image)
 
-amplify_rec('data/denoising/F16_GT.png', (220, 210), (350, 320))
+amplify_rec('optimal for lena/optimal_gt_sm_pgd_ad.png', 'amp_lena/optimal_gt_sm_pgd_ad.png', (180, 160), (310, 270))
+amplify_rec('optimal for lena/optimal_gt_sm_dip.png', 'amp_lena/optimal_gt_sm_dip.png', (180, 160), (310, 270))
+amplify_rec('optimal for lena/optimal_gt_dip.png', 'amp_lena/optimal_gt_dip.png', (180, 160), (310, 270))
+amplify_rec('optimal for lena/optimal_gt_pgd_ad.png', 'amp_lena/optimal_gt_pgd_ad.png', (180, 160), (310, 270))
+amplify_rec('optimal for lena/noisy_lena.png', 'amp_lena/noisy_lena.png', (180, 160), (310, 270))
---
+++ optimal for lena/readme.txt
@@ -0,0 +1,10 @@
+DIP:
+optimal gt:
+runtime: 362.69443917274475 iteration: 2300 psnr: 30.582339706197427
+optimal gt sm:
+runtime: 378.52670097351074 iteration: 2400 psnr sm: 32.60916169814712
+PGD_AD:
+optimal gt:
+runtime: 168.51438665390015 iteration: 1000 psnr: 30.3108104316582
+optimal gt sm:
+runtime: 218.2996277809143 iteration: 1300 psnr sm: 31.804489970874986
